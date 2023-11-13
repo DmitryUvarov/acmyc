3f02cb1f9d4d3504b027c469dc16411bbe722213
Dima <uvarov3123@gmail.com>
2023-11-13 13:37:13 +0200

    add header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,16 +3,22 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-// window.addEventListener('load', pageLoad)
+window.addEventListener('load', pageLoad)
 
-// function pageLoad() {
-//     const htmlTag = document.documentElement
+function pageLoad() {
+    const htmlTag = document.documentElement
 
-//     document.addEventListener('click', e => {
-//         const targetElement = e.target
+    document.addEventListener('click', e => {
+        const targetElement = e.target
 
-//         if (targetElement.closest('.class')) {
-//             console.log('1');
-//         }
-//     })
-// }
+        if (targetElement.closest('.menu__item')) {
+            if (document.querySelector('.menu__item.open')) {
+                document.querySelector('.menu__item.open').classList.remove('open')
+            }
+            targetElement.closest('.menu__item').classList.toggle('open')
+        }
+        if (!targetElement.closest('.menu__item') && document.querySelector('.menu__item.open')) {
+            document.querySelector('.menu__item.open').classList.remove('open')
+        }
+    })
+}
